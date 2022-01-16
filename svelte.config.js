c59ebb13a1ce5d66d8f4f045136dc1c267881a78
Riccardo Persiani <r.persiani92@gmail.com>
2022-01-16 15:16:22 +0100

    add new dist

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,9 @@
 import preprocess from "svelte-preprocess";
-import adapter from '@sveltejs/adapter-static';
 
 const config = {
   preprocess: [preprocess({
     postcss: true
   })],
-  kit: {
-		target: '#svelte',
-		adapter: adapter({
-			pages: 'build',  // path to public directory
-			assets: 'build',  // path to public directory
-			fallback: null
-		})
-	}
 };
 
 export default config;
