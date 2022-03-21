245290b1066e88a08eba1a9c12abe9f91729fd6a
Riccardo Persiani <r.persiani92@gmail.com>
2022-03-21 15:46:37 +0100

    fix: base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: 'ensonsteroids.xyz'
+  base: '/riccardopersiani.github.io'
 })
 
