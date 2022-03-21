8bf618af5f1003d3266546d5e2b06c8dbf4f851a
Riccardo Persiani <r.persiani92@gmail.com>
2022-03-21 16:00:07 +0100

    update cname

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ ghpages.publish(
 	'public', // path to public directory
 	{
 		branch: 'gh-pages',
-		repo: 'https://github.com/riccardopersiani/ensonsteroids.github.io.git', // Update to point to your repository
+		repo: 'https://github.com/riccardopersiani/riccardopersiani.github.io.git', // Update to point to your repository
 		user: {
 			name: 'Riccardo Persiani', // update to use your name
 			email: 'r.persiani92@gmail.com' // Update to use your email
